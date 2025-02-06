32966358f0c602e1108f57afa24d7acffb2d08f8
abderahman2651 <abderahmanmahmoud2651@gmail.com>
2025-02-06 09:45:14 +0200

    شرح سريع عن التعديلات اللي عملتها

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -38,3 +38,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WeatherAppTheme {
+        Greeting("Android")
+    }
+}
